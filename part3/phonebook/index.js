22d1ffbb607428c0685448380f4c846b74ee45f1
Tunç Başar Köse <tuncbasark@gmail.com>
2022-12-21 12:23:29 +0300

    3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,15 @@
 const express = require("express")
 const morgan = require("morgan")
+morgan.token("body", (req, res) => {
+    if (req.method === "POST"){
+        return ( JSON.stringify(req.body) )
+    }
+    return(" ")
+})
 
 const app = express()
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms :body"))
 
 const generateId = () => Math.floor(Math.random() * 10000)
 

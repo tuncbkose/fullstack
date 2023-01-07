8192d8d62bb117541180a69d71cbdfb3e5742520
Tunç Başar Köse <tuncbasark@gmail.com>
2023-01-07 15:05:30 +0200

    3.15

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -33,14 +33,25 @@ app.get("/info", (request, response) => {
 
 app.get("/api/persons/:id", (request, response) => {
     Person.findById(request.params.id).then(
-        person => response.json(person)
-    )
+        person => {
+            if (person){
+                response.json(person)
+            } else {
+                response.status(404).end()
+            }
+        }
+    ).catch(error => {
+        console.log(error)
+        response.status(400).send({error: 'malformatted id'})
+    })
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
+app.delete("/api/persons/:id", (request, response, next) => {
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post("/api/persons", (request, response) => {

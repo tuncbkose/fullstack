77b8c3c17d3602adeb245e9c88e461c0bcc8ee56
Tunç Başar Köse <tuncbasark@gmail.com>
2023-06-25 14:30:06 +0300

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,5 +1,10 @@
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+    return blogs.length === 0 ? 0 : blogs.reduce((acc, blog) => acc+blog.likes, 0)
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes
 }

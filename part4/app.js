4c01e29ad6126768d8bb479ef52afcbe40386460
Tunç Başar Köse <tuncbasark@gmail.com>
2023-06-25 13:16:06 +0300

    4.2

---
+++ part4/app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const blogRouter = require('./controllers/blogs')
+const config = require('./utils/config')
+
+
+mongoose.connect(config.MONGODB_URI)
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogRouter)
+
+
+
+
+module.exports = app

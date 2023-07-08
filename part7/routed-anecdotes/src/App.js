6c17fc84144837e2f8569f370f359a138ecaf834
Tunç Başar Köse <tuncbasark@gmail.com>
2023-07-08 16:49:23 +0300

    7.5/6

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -6,6 +6,7 @@ import {
     useMatch,
     useNavigate
 } from "react-router-dom";
+import  { useField } from './hooks'
 
 const Menu = () => {
   const padding = {
@@ -64,21 +65,27 @@ const Footer = () => (
 )
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState('')
-  const [author, setAuthor] = useState('')
-  const [info, setInfo] = useState('')
+  const content = useField('text')
+  const author = useField('text')
+  const info = useField('text')
 
   const navigate = useNavigate()
 
   const handleSubmit = (e) => {
     e.preventDefault()
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0
     })
     navigate('/')
+  }
+
+  const resetAll = () => {
+      content.reset()
+      author.reset()
+      info.reset()
   }
 
   return (
@@ -87,17 +94,27 @@ const CreateNew = (props) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input name='content' value={content} onChange={(e) => setContent(e.target.value)} />
+          <input
+              name='content'
+              value={content.value}
+              onChange={content.onChange} />
         </div>
         <div>
           author
-          <input name='author' value={author} onChange={(e) => setAuthor(e.target.value)} />
+          <input
+              name='author'
+              value={author.value}
+              onChange={author.onChange} />
         </div>
         <div>
           url for more info
-          <input name='info' value={info} onChange={(e)=> setInfo(e.target.value)} />
+          <input
+              name='info'
+              value={info.value}
+              onChange={info.onChange} />
         </div>
         <button>create</button>
+        <button type='button' onClick={resetAll}>reset</button>
       </form>
     </div>
   )

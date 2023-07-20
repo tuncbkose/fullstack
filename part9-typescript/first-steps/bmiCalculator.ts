7d8c4a1bdede1ff44c7eb8a3d36689dce2bce97e
Tunç Başar Köse <tuncbasark@gmail.com>
2023-07-20 11:26:36 +0300

    9.3

--- part9-typescript/first-steps/bmiCalculator.ts
+++ part9-typescript/first-steps/bmiCalculator.ts
@@ -1,3 +1,24 @@
+interface BmiInput {
+    heightInCm: number;
+    weightInKg: number;
+}
+
+const parseBmiArguments = (args: string[]): BmiInput => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+    const heightInCm = Number(args[2]);
+    const weightInKg = Number(args[3]);
+    if (!isNaN(heightInCm) && !isNaN(weightInKg)) {
+        return {
+            heightInCm,
+            weightInKg
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+
+}
+
 const calculateBmi = (heightInCm: number, weightInKg: number): string => {
     const ratio = weightInKg / (heightInCm / 100)**2;
     if (ratio < 18.5) return "Underweight";
@@ -5,4 +26,11 @@ const calculateBmi = (heightInCm: number, weightInKg: number): string => {
     return "Normal (healthy weight)";
 }
 
-console.log(calculateBmi(180, 74));
+try {
+    const { heightInCm, weightInKg } = parseBmiArguments(process.argv);
+    console.log(calculateBmi(heightInCm, weightInKg));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+}
--- part9-typescript/first-steps/exerciseCalculator.ts
+++ part9-typescript/first-steps/exerciseCalculator.ts
@@ -1,3 +1,8 @@
+interface ExerciseInput {
+    target: number;
+    hours: number[];
+}
+
 interface Result {
     periodLength: number;
     trainingDays: number;
@@ -8,18 +13,38 @@ interface Result {
     ratingDescription: string;
 }
 
+const parseExerciseArguments = (args: string[]): ExerciseInput => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    const [target, ...hours] = args.slice(2).map(s => Number(s));
+    if (!isNaN(target) && !hours.some(isNaN)){
+        return {
+            target,
+            hours
+        }
+    } else {
+        throw new Error('Provided values were not numbers!');
+    }
+}
+
 const calculateExercises = (hours: number[], target: number): Result => {
     const average = hours.reduce((acc, num) => acc+num) / hours.length;
     const trainingDays = hours.reduce((acc, num) => acc+Number(num!==0), 0);
     return {
         periodLength: hours.length,
-        trainingDays: trainingDays,
-        target: target,
-        average: average,
+        trainingDays,
+        target,
+        average,
         success: average >= target,
         rating: 3,
         ratingDescription: "could be better"
     }
 }
 
-console.log(calculateExercises([3, 0, 2, 4.5, 0, 3, 1], 2));
+try {
+    const { target, hours } = parseExerciseArguments(process.argv);
+    console.log(calculateExercises(hours, target));
+} catch (error: unknown) {
+    if (error instanceof Error) {
+        console.log(error.message);
+    }
+}

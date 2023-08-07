b015376cfe9b4037d2d03b57b2e102872d641465
Tunç Başar Köse <tuncbasark@gmail.com>
2023-08-07 11:25:43 +0300

    9.15

--- part9-typescript/ts-react/src/App.tsx
+++ part9-typescript/ts-react/src/App.tsx
@@ -6,21 +6,126 @@ const Header = (props: HeaderProps) => {
     return <h1>{props.name}</h1>
 }
 
-interface Part {
+interface CoursePartBase {
     name: string;
     exerciseCount: number;
 }
 
-const Part = (props: Part) => {
-    return (
-        <p>
-            {props.name} {props.exerciseCount}
-        </p>
-    )
+interface CoursePartWithDescription extends CoursePartBase {
+    description: string;
+}
+
+interface CoursePartBasic extends CoursePartWithDescription {
+  kind: "basic"
+}
+
+interface CoursePartGroup extends CoursePartBase {
+  groupProjectCount: number;
+  kind: "group"
+}
+
+interface CoursePartBackground extends CoursePartWithDescription {
+  backgroundMaterial: string;
+  kind: "background"
+}
+
+interface CoursePartWithReqs extends CoursePartWithDescription {
+    kind: "special";
+    requirements: string[];
+}
+
+type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground | CoursePartWithReqs;
+
+const courseParts: CoursePart[] = [
+  {
+    name: "Fundamentals",
+    exerciseCount: 10,
+    description: "This is an awesome course part",
+    kind: "basic"
+  },
+  {
+    name: "Using props to pass data",
+    exerciseCount: 7,
+    groupProjectCount: 3,
+    kind: "group"
+  },
+  {
+    name: "Basics of type Narrowing",
+    exerciseCount: 7,
+    description: "How to go from unknown to string",
+    kind: "basic"
+  },
+  {
+    name: "Deeper type usage",
+    exerciseCount: 14,
+    description: "Confusing description",
+    backgroundMaterial: "https://type-level-typescript.com/template-literal-types",
+    kind: "background"
+  },
+  {
+    name: "TypeScript in frontend",
+    exerciseCount: 10,
+    description: "a hard part",
+    kind: "basic",
+  },
+  {
+  name: "Backend development",
+  exerciseCount: 21,
+  description: "Typing the backend",
+  requirements: ["nodejs", "jest"],
+  kind: "special"
+  }
+];
+
+/**
+ * Helper function for exhaustive type checking
+ */
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
+
+const Part = (props: CoursePart) => {
+    switch (props.kind) {
+        case "basic":
+            return (
+                <p>
+                    {props.name} {props.exerciseCount} <br/>
+                    {props.description}
+                </p>
+            )
+        case "background":
+            return (
+                <p>
+                    {props.name} {props.exerciseCount} <br/>
+                    {props.description} <br/>
+                    Background material: {props.backgroundMaterial}
+                </p>
+            )
+        case "group":
+            return (
+                <p>
+                    {props.name} {props.exerciseCount} <br/>
+                    project exercises {props.groupProjectCount}
+                </p>
+            )
+        case "special":
+            return (
+                <p>
+                    {props.name} {props.exerciseCount} <br/>
+                    {props.description} <br/>
+                    required skills: {props.requirements.join(" ")}
+                </p>
+            )
+        default:
+            return assertNever(props);
+    }
 }
 
 interface ContentProps {
-    parts: Part[];
+    parts: CoursePart[];
 }
 
 const Content = (props: ContentProps) => {
@@ -42,20 +147,6 @@ const Total = (props: ContentProps) => {
 
 const App = () => {
   const courseName = "Half Stack application development";
-  const courseParts = [
-    {
-      name: "Fundamentals",
-      exerciseCount: 10
-    },
-    {
-      name: "Using props to pass data",
-      exerciseCount: 7
-    },
-    {
-      name: "Deeper type usage",
-      exerciseCount: 14
-    }
-  ];
 
   return (
     <div>

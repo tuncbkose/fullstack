446b1a44da7770d1a6b7999a531fde12c9987703
Tunç Başar Köse <tuncbasark@gmail.com>
2023-08-08 13:49:39 +0300

    9.19

--- part9-typescript/flight-diary-frontend/src/components/EntryForm.tsx
+++ part9-typescript/flight-diary-frontend/src/components/EntryForm.tsx
@@ -2,7 +2,7 @@ import {EntryFormProps, NewDiaryEntry, Visibility, Weather} from "../types";
 import React, {useState} from "react";
 
 
-const emptyEntry = {
+const defaultEntry = {
     date: '',
     visibility: '',
     weather: '',
@@ -10,7 +10,7 @@ const emptyEntry = {
 }
 
 const EntryForm = (props: EntryFormProps) => {
-    const [newEntry, setNewEntry] = useState(emptyEntry);
+    const [newEntry, setNewEntry] = useState(defaultEntry);
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setNewEntry({
@@ -21,27 +21,58 @@ const EntryForm = (props: EntryFormProps) => {
 
     const handleSubmit = (event: React.SyntheticEvent) => {
         event.preventDefault()
-        const entryToAdd: NewDiaryEntry = {
-            ...newEntry,
-            visibility: Visibility[newEntry.visibility as keyof typeof Visibility],
-            weather: Weather[newEntry.weather as keyof typeof Weather],
-        }
+        const entryToAdd = newEntry as NewDiaryEntry
         props.createEntry(entryToAdd)
-        setNewEntry(emptyEntry)
+        setNewEntry(defaultEntry)
     }
 
     return (
         <>
             <form onSubmit={handleSubmit}>
-                date <input value={newEntry.date}
+                date <input type="date"
+                       value={newEntry.date}
                        name='date'
                        onChange={handleChange}/> <br/>
-                visibility <input value={newEntry.visibility}
+
+                visibility
+                <input type='radio'
                        name='visibility'
-                       onChange={handleChange}/> <br/>
-                weather <input value={newEntry.weather}
+                       onChange={() => setNewEntry({...newEntry, visibility: Visibility.Great})}
+                       required/> Great
+                <input type='radio'
+                       onChange={() => setNewEntry({...newEntry, visibility: Visibility.Good})}
+                       name='visibility'/> Good
+                <input type='radio'
+                       onChange={() => setNewEntry({...newEntry, visibility: Visibility.Ok})}
+                       name='visibility'/> Ok
+                <input type='radio'
+                       onChange={() => setNewEntry({...newEntry, visibility: Visibility.Poor})}
+                       name='visibility'/> Poor
+                <br/>
+
+                weather
+                <input type='radio'
                        name='weather'
-                       onChange={handleChange}/> <br/>
+                       onChange={() => setNewEntry({...newEntry, weather: Weather.Sunny})}
+                       required/> Sunny
+                <input type='radio'
+                       name='weather'
+                       onChange={() => setNewEntry({...newEntry, weather: Weather.Rainy})}
+                       /> Rainy
+                <input type='radio'
+                       name='weather'
+                       onChange={() => setNewEntry({...newEntry, weather: Weather.Cloudy})}
+                       /> Cloudy
+                <input type='radio'
+                       name='weather'
+                       onChange={() => setNewEntry({...newEntry, weather: Weather.Stormy})}
+                       /> Stormy
+                <input type='radio'
+                       name='weather'
+                       onChange={() => setNewEntry({...newEntry, weather: Weather.Windy})}
+                       /> Windy
+                <br/>
+
                 comment <input value={newEntry.comment}
                        name='comment'
                        onChange={handleChange}/> <br/>
